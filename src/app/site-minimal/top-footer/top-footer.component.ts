79ff07917cca242662ba6b80ab9a337000de03ca
Mateo Gomez Ortega <mgomezortega@gmail.com>
2019-03-21 16:22:15 -0300

    las argentinas footer-branding

--- src/app/site-minimal/top-footer/top-footer.component.ts
+++ src/app/site-minimal/top-footer/top-footer.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { RecordCard } from '../recordcard.model';
 
 import { MainMenuItem, SocialMediaItem } from '../menu-helper';
 import { MinimalMenuService } from '../minimal-menu.service';
 import { gldef } from '../../develar-commons/develar.config';
+
+const BRANDING = 'branding';
+const FOOTER = 'footer';
 
 
 @Component({
@@ -15,13 +20,25 @@ export class TopFooterComponent implements OnInit {
 	@Input() record: RecordCard;
 
 	public mainimage: string = "";
-	public nodes: Array<Footer> = [];
+	public bnodes: Array<Footer> = [];
+  public fnodes: Array<Footer> = [];
+  
   public isModoUNO = false;
   public isModoDOS = true;
   public socialItems: SocialMediaItem[];
 
+
+  private hasActiveUrlPath = false;
+  private actualUrl = "";
+  private navigationUrl = "";
+
+  public isHomeView = false;
+
+
   constructor(
-    private mainMenuService: MinimalMenuService) {
+    private mainMenuService: MinimalMenuService,
+    private route: ActivatedRoute,
+    private router: Router) {
 
         mainMenuService.socialListener$.subscribe(
           items => {
@@ -32,12 +49,32 @@ export class TopFooterComponent implements OnInit {
      }
 
   ngOnInit() {
+
+    this.route.url.subscribe(url=> {
+
+      this.actualUrl = this.router.routerState.snapshot.url;
+      this.actualUrl = this.actualUrl ? this.actualUrl.split('?')[0] : this.actualUrl;
+
+      if(!this.actualUrl || this.actualUrl === "/"){
+        this.setupHomeView(true);
+
+      } else {
+        this.setupHomeView(false);
+
+      }
+
+    });
+
+
+
   	this.mainimage = this.record.mainimage;
 
     this.mainMenuService.loadSocialItems(gldef.socialmedia)
 
   	this.record.relatedcards.forEach(s => {
-      let link:string , navigate:string , noLink = true;
+      let link:string , navigate:string , noLink = true, isFooter = true, isBranding = false;
+
+      if(s.description === "<p><br></p>") s.description = "";
 
       if(s.linkTo){
         noLink = false;
@@ -49,23 +86,82 @@ export class TopFooterComponent implements OnInit {
         }
       }
 
-  		this.nodes.push({
-        imageUrl: s.mainimage,
-        description: s.description,
-        linkTo: link,
-        navigateTo: navigate,
-        noLink: noLink,
-        hasImage: !(s.mainimage === "")
-  		} as Footer)
+      if(s.topic == BRANDING){
+        console.log('isBranding')
+        isBranding = true;
+        isFooter = false;
+        this.bnodes.push({
+          imageUrl: s.mainimage,
+          description: s.description,
+          linkTo: link,
+          type: s.topic,
+          isBranding: isBranding,
+          isFooter: isFooter,
+          navigateTo: navigate,
+          noLink: noLink,
+          hasImage: !(s.mainimage === "")
+        } as Footer)
+
+
+      }else if (s.topic == FOOTER){
+        console.log('isFOOTER')
+        isBranding = false;
+        isFooter = true;
+
+        this.fnodes.push({
+          imageUrl: s.mainimage,
+          description: s.description,
+          linkTo: link,
+          type: s.topic,
+          isBranding: isBranding,
+          isFooter: isFooter,
+          navigateTo: navigate,
+          noLink: noLink,
+          hasImage: !(s.mainimage === "")
+        } as Footer)
+
+
+
+      }else{
+        this.fnodes.push({
+          imageUrl: s.mainimage,
+          description: s.description,
+          linkTo: link,
+          type: s.topic,
+          isBranding: isBranding,
+          isFooter: isFooter,
+          navigateTo: navigate,
+          noLink: noLink,
+          hasImage: !(s.mainimage === "")
+        } as Footer)
+
+      }
+
+
   	})
   }
+
+  setupHomeView(isHome){
+    if(isHome){
+      this.isHomeView = true;
+
+    }else{
+      this.isHomeView = false;
+    }
+
+  }
+
+
 }
 
 interface Footer {
 	imageUrl: string;
 	description: string;
+  type: string;
   linkTo: string;
   navigateTo: string;
   noLink: boolean;
+  isBranding: boolean;
+  isFooter: boolean;
   hasImage: boolean;
 }
