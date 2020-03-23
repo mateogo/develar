7f232b5e427ac221b05f9de8978e7cab791463d0
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-03-23 14:14:48 -0300

    salud: ts fecha alta y um en table

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '© Copyright 2020 - DGTS@modernización - Secretaría de Salud - Municipalidad de Almte. Brown (v1.0-20.03.23-13:26) ',
+		copyright: '© Copyright 2020 - DGTS@modernización - Secretaría de Salud - Municipalidad de Almte. Brown (v1.0-20.03.23-14:22) ',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
