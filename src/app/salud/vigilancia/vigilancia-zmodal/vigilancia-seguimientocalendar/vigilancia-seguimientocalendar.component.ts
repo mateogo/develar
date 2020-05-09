9b483c427b4dd82dd895f4b325f48c2ccc6aa06e
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-05-09 12:18:03 -0300

    salud mejoras en el calendario

--- src/app/salud/vigilancia/vigilancia-zmodal/vigilancia-seguimientocalendar/vigilancia-seguimientocalendar.component.ts
+++ src/app/salud/vigilancia/vigilancia-zmodal/vigilancia-seguimientocalendar/vigilancia-seguimientocalendar.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, ViewChild, OnInit, Inject } from '@angular/core';
 
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { devutils }from '../../../../develar-commons/utils';
 
+import { FullCalendarComponent } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { EventInput } from '@fullcalendar/core';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
 
 import { SaludController } from '../../../salud.controller';
 
 import {   Asistencia, AfectadoFollowUp, AfectadoUpdate, UpdateAsistenciaEvent,
           AsistenciaHelper } from '../../../asistencia/asistencia.model';
+
 
 const UPDATE = 'update';
 const BROWSE = 'browse';
@@ -22,20 +27,26 @@ const SISA_VIEW = 'sisa:view';
   styleUrls: ['./vigilancia-seguimientocalendar.component.scss']
 })
 export class VigilanciaSeguimientocalendarComponent implements OnInit {
+  @ViewChild('calendar', {static: false}) calendarComponent: FullCalendarComponent; // the #calendar in the template
+
+  public calendarVisible = false;
+  public calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
+  public calendarWeekends = true;
+
 
   public asistencia: Asistencia;
   public token: AfectadoUpdate; // from DB
   public tokenData: TokenData; // template usage
 
 	public tokenList: AfectadoUpdate[] = [];
-	public eventList: CalendarData[] = [];
+	public eventList: EventInput[] = [];
 
   private result: UpdateAsistenciaEvent;
 
   public displayAs = '';
 
   public showAfectadoFollowUp = false;
-  public calendarPlugins = [dayGridPlugin];
+  private inicioDate: string = null;
 
  
   constructor(
@@ -57,6 +68,32 @@ export class VigilanciaSeguimientocalendarComponent implements OnInit {
   								} as  UpdateAsistenciaEvent;
   }
 
+
+  toggleVisible() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  toggleWeekends() {
+    this.calendarWeekends = !this.calendarWeekends;
+  }
+
+  gotoPast() {
+  	if(!this.inicioDate) return;
+    let calendarApi = this.calendarComponent.getApi();
+    calendarApi.gotoDate(this.inicioDate); // call a method on the Calendar object
+  }
+
+  handleDateClick(arg) {
+    if (confirm('Funcionalidad aún no implementada ' + arg.dateStr + ' ?')) {
+      this.eventList = this.eventList.concat({ // add new event data. must create new array
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    }
+  }
+
+
   private buildAfectadoFollowUp(entity: Asistencia){
     this.displayAs = this.asistencia.requeridox ? this.asistencia.requeridox.slug + ' ' + (this.asistencia.telefono || '') : '';
 
@@ -67,21 +104,35 @@ export class VigilanciaSeguimientocalendarComponent implements OnInit {
     this.tokenData = new TokenData(entity.followUp);
 
     this.tokenList = entity.seguimEvolucion || [];
-    this.buildTokenEvents(this.tokenList);
+    this.buildTokenEvents(this.tokenList, entity.followUp, entity);
     this.sortProperly(this.tokenList);
 
     this.showAfectadoFollowUp = true;
+    this.calendarVisible = true;
   }
 
 
-  private buildTokenEvents(list: AfectadoUpdate[]){
+  private buildTokenEvents(list: AfectadoUpdate[], followUp: AfectadoFollowUp, sinternacion: Asistencia){
   	this.eventList = list.map(t =>{
   		let dateTx = devutils.txInvertedFromDate(new Date(t.fets_llamado));
   		return {
   			title: `LL: ${t.resultado}: ${t.vector}`,
-  			date: dateTx
-  		} as CalendarData
+  			start: dateTx
+  		} as EventInput
   	})
+
+  	if(followUp && followUp.fets_inicio){
+  		let hisopado = this.filterNecesidadDeLaboratorio(sinternacion)
+  		let hisopadoYa = hisopado.needsHisopado ? ' ¡HISOPAR! ' : '';
+  		let covid = hisopado.isCovid ? 'COVID' : '';
+  		this.inicioDate = devutils.txInvertedFromDate(new Date(followUp.fets_inicio));
+
+	  	this.eventList.push({
+	  			title: `Seguimiento: ${followUp.tipo} Días:#${hisopado.dias}  ${covid}  ${hisopadoYa} `,
+	  			start: devutils.txInvertedFromDate(new Date(followUp.fets_inicio)), 
+	  			end: devutils.txInvertedFromDate(new Date()), 
+	  		} as EventInput)
+  	}
 
   }
 
@@ -101,11 +152,79 @@ export class VigilanciaSeguimientocalendarComponent implements OnInit {
     this.dialogRef.close(this.result);
   }
 
+	private filterNecesidadDeLaboratorio(token: Asistencia): HisopadoYa{
+		let response = new HisopadoYa();
+		let today = new Date()
+		let today_ts = today.getTime();
+		let feReferencia_ts = null;
+		let isCovid = false;
+
+		// caso 1
+		if(!(token.followUp)) return response;
+		feReferencia_ts = token.followUp.fets_inicio;
+		response.hasFollowUp = true;
+		let diasSeguimiento = (today_ts - feReferencia_ts) / (1000 * 60 * 60 * 24) ;
+		response.dias = Math.floor(diasSeguimiento);
+
+		if(token.infeccion){
+		  if(token.infeccion.actualState === 1){
+		    isCovid = true;
+		    response.isCovid = true;
+		    feReferencia_ts = token.infeccion.fets_confirma
+				diasSeguimiento = (today_ts - feReferencia_ts) / (1000 * 60 * 60 * 24) ;
+				response.dias = Math.floor(diasSeguimiento);
+
+		  }
+
+		  // caso 2
+		  if(token.infeccion.actualState === 3 || token.infeccion.actualState === 4 || token.infeccion.actualState === 5   ) return response;
+		}
+
+		if(diasSeguimiento< 14) return response;
+
+
+		if(token.muestraslab && token.muestraslab.length){
+		  let muestras = token.muestraslab;
+		  let fechaLab = null
+
+		  muestras.forEach(mu => {
+		    if(mu.resultado === 'descartada') {
+		      fechaLab = mu.fets_resestudio
+		    }
+		  })
+
+		  if(fechaLab){
+		      if((today_ts - fechaLab) / (1000 * 60 * 60 * 24) < 2) return response;
+		      else {
+		      	response.needsHisopado = true;
+		      	return response;
+		      }
+
+		  }else{
+		  	response.needsHisopado = true;
+		    return response;
+		  }
+		  
+		}else{
+			response.needsHisopado = true;
+		  return response;
+		}
+
+	}
+}
+
+class HisopadoYa {
+	dias: number = 0;
+	hasFollowUp = false;
+	isCovid = false;
+	needsHisopado = false;	
 }
 
 class CalendarData {
 	title: string = '';
-	date: string = '';
+	start: string = '';
+	end: string = '';
+	allDay: string = ''
 }
 
 class TokenData {
