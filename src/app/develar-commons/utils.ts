7f24d7fb1be8f690a54e17381b79d820043d9c13
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-05-20 23:32:34 -0300

    dsocial feriado 25 de mayo

--- src/app/develar-commons/utils.ts
+++ src/app/develar-commons/utils.ts
@@ -420,7 +420,7 @@ function getEpidemicWeekOfTheYear(fecharef:Date ): number{
 }
 
 // feriados
-const inactivosList = ["31/03/2020", "10/04/2020", "01/05/2020"]
+const inactivosList = ["31/03/2020", "10/04/2020", "01/05/2020", "25/05/2020"]
 const laborDayOfWeek = [0, 1, 1, 1, 1, 1, 0]
 
 function nextLaborDay(date: Date, offset): Date{
