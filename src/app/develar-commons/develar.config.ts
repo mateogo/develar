d74521eb848db83bc0e2edf34aa7ffde91bb4b0f
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-06-04 18:30:13 -0300

    salud d3 informe grafo contactos 02

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #22 20.06.03-18:43',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #22 20.06.04-18:28',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
