588aaaffdce875dfbc4f935403b69aaad2f2334c
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-06-23 09:35:49 -0300

    salud gestion de llamados nueva version 01

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #26 20.06.22-14:06',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #26 20.06.23-09:18',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
