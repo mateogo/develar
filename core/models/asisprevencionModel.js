8be7700898d96fd0de0a92fcc0f6954489524b5a
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-07-08 02:16:24 -0300

    salud actualizacion en produccion luego del upgrade a Angular 9-003

--- core/models/asisprevencionModel.js
+++ core/models/asisprevencionModel.js
@@ -1206,7 +1206,7 @@ function geolocalizacionContactos(movimientos, query, errcb, cb){
 
 function checkForLatLon(list){
   let start = 0;
-  let end = 50;
+  let end = 3;
   let once = 0;
   list.forEach(token => {
     if((token.lat === -34.8112108487836 || token.lon ===  -58.3581617661068) && (token.citY !== 'S/D' && token.street1 !== 'S/D')){
