692362d0b44a426f6a33c4f5c2bd0c671cf99909
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-07-08 02:02:38 -0300

    salud actualizacion en produccion luego del upgrade a Angular 9-002

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoiZmFjdW5kbzE5OThqbSIsImEiOiJjazk0ZHltNjcwYzUzM29tczNjc2Y0MjNmIn0.m9yhZsjJv-QHBYlpAuDD7A'
+  
 };
