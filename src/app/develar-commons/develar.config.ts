dc26e705fec924ba067c0ad7f1b2628eb82c77f9
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-07-23 07:17:44 -0300

    salud: ajustes tablero internacion 05

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #29 20.07.23-06:03',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #29 20.07.23-06:04',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
