36333c5041a31c7dbb08f43ff79b61ed2d16eac9
milton-sesarego <milton.sesarego@gmail.com>
2020-08-03 13:51:34 -0300

    incorporación de canvas al reporte de grafo

--- src/app/develar-commons/d3/force-directed-graph/force-directed-graph.component.ts
+++ src/app/develar-commons/d3/force-directed-graph/force-directed-graph.component.ts
@@ -1,266 +1,196 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, OnInit, Input, ViewEncapsulation, ElementRef , AfterViewInit} from '@angular/core';
 import { Observable } from 'rxjs';
 
 import * as d3 from 'd3';
-//import {swatches} from "@d3/color-legend"
 
 @Component({
   selector: 'force-directed-graph',
   templateUrl: './force-directed-graph.component.html',
   styleUrls: ['./force-directed-graph.component.scss'],
   encapsulation: ViewEncapsulation.None
-
 })
-export class ForceDirectedGraphComponent implements OnInit {
-	@Input() data:Array<any> = [];
-	@Input() parameter$: Observable<string>;
+export class ForceDirectedGraphComponent implements AfterViewInit {
+  @ViewChild('canvasEl', {static: false}) canvasEl: ElementRef;
+  private ctx: CanvasRenderingContext2D;
 
-	public showGraph = true;
-	private defaultView = true;
-
-	public chart: any;
-
-	private actualParam = 'no_definido';
-
-	links: any;
-	nodes: any;
-
-	simulation: any;
-	invalidation: any;
-	link: any;
-	node: any;
-	svg: any;
-	types = ['licensing', 'suit', 'resolved']
-	height = 3500;
-	width = 2400;
-	color: any;
+  @Input() data: Array<any> = [];
+  @Input() parameter$: Observable<string>;
+  public showGraph = true;
+  private defaultView = true;
+  private actualParam = 'no_definido';
+  canvas;
+  simulation;
+  links; nodes;
+  link; node; types;
+  height = 800;
+  width = 800;
+  nodeRadius = 5;
+  transform;
+  color;
+  font = 20;
 
   constructor() { }
 
-  ngOnInit() {
-  	this.initData();
-	  this.color = d3.scaleOrdinal(d3.schemeCategory10)
+  ngAfterViewInit() {
+    this.color = d3.scaleOrdinal(d3.schemeCategory10);
+    this.ctx = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.canvas = this.ctx.canvas;
+    this.canvas.style.width = '100%';
+    this.canvas.style.height = '100%';
 
-  	if(this.parameter$){
-  		this.parameter$.subscribe(param => {
-  			this.refreshGraph(param)
-  		})
-  	}
+    this.canvas.width  = this.canvas.offsetWidth;
+    this.canvas.height = this.height;
+    this.width = this.canvas.width;
 
-	  //this.resetGraph();
-
-
-
+    if (this.parameter$){
+      this.parameter$.subscribe(param => {
+        this.refreshGraph(param);
+      });
+    }
   }
 
-  private resetGraph(){
-  	if(this.defaultView){
-  		this.height = 2500;
-  		this.width =  1900;
+  forceGraph() {
+    this.transform = d3.zoomIdentity;
+    const self = this;
+    d3.select(this.canvas)
+      .call(d3.drag()
+            .container(this.canvas)
+            .subject(dragSubject)
+            .on('start', dragStarted)
+            .on('drag', dragged)
+            .on('end', dragEnded))
+      .call(d3.zoom()
+            .scaleExtent([1 / 10, 8])
+            .on('zoom', zoomed));
 
-  	}else {
-  		this.height = 1200;
-  		this.width =  1200;
-
-  	}
-
-
-  	this.simulation = d3.forceSimulation(this.nodes)
+    this.simulation = d3.forceSimulation(this.nodes)
       .force("link", d3.forceLink( this.links).id( d => d['id']) )
       .force("charge", d3.forceManyBody().strength(-300))
-      //.force("center", d3.forceCenter(this.width/2, this.height/2));
+      .force("center", d3.forceCenter(this.width/2, this.height/2))
       .force("x", d3.forceX())
-      .force("y", d3.forceY());
+      .force("y", d3.forceY())
+      .on("tick", simulationUpdate);
 
-    this.svg = d3.select('#graph').select('svg')
-    	.attr("viewBox", `${-this.width / 2} ${-this.height / 2} ${ this.width} ${this.height}`)
-      .style("font", "12px sans-serif");
+    function simulationUpdate() {
+      var fontSize = self.font * self.transform[2];
+      self.ctx.save();
+      self.ctx.clearRect(0, 0, self.width, self.height);
+      self.ctx.translate(self.transform.x, self.transform.y);
+      self.ctx.scale(self.transform.k, self.transform.k);
 
-  	// Per-type markers, as they don't inherit styles.
-  	this.svg.append("defs").selectAll("marker")
-	    .data(this.types)
-	    .join("marker")
-	      .attr("id", d => `arrow-${d}`)
-	      .attr("viewBox", "0 -5 10 10")
-	      .attr("refX", 15)
-	      .attr("refY", -0.5)
-	      .attr("markerWidth", 6)
-	      .attr("markerHeight", 6)
-	      .attr("orient", "auto")
-	    .append("path")
-	      .attr("fill", this.color)
-	      .attr("d", "M0,-5L10,0L0,5");
+      // Draw edges
+      self.links.forEach(function(d) {
+        self.ctx.beginPath();
+        self.ctx.moveTo(d.source.x, d.source.y);
+        self.ctx.lineTo(d.target.x, d.target.y);
+        self.ctx.lineWidth = Math.sqrt(d.value);
+        self.ctx.strokeStyle = self.color(d.type);
+        self.ctx.lineWidth = 1.5;
+        self.ctx.stroke();
 
-  	this.link = this.svg.append("g")
-				.attr("fill", "none")
-				.attr("stroke-width", 1.5)
-				.selectAll("path")
-				.data(this.links)
-				.join("path")
-				.attr("stroke", d => this.color(d.type))
-				.attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, "http://develar-local.co:4200/salud/gestion/reportesvigilancia")})`);
+      });
+      // Draw nodes
+      self.nodes.forEach(function(d, i) {
+        self.ctx.textAlign = 'center';
+        self.ctx.beginPath();
+        // Node fill
+        self.ctx.moveTo(d.x + self.nodeRadius, d.y);
+        self.ctx.arc(d.x, d.y, self.nodeRadius, 0, 2 * Math.PI);
+        // Node outline
+        self.ctx.strokeStyle = '#000';
+        self.ctx.lineWidth = 1.5;
+        self.ctx.stroke();
+        self.ctx.fillStyle = '#fff';
+        self.ctx.fill();
+        // Text
+        self.ctx.font = 'bold ' + (fontSize) + 'px sans-serif"';
+        self.ctx.strokeStyle = 'white';
+        self.ctx.lineWidth = 2;
+        self.ctx.lineJoin = 'miter';
+        self.ctx.miterLimit = 2;
+        self.ctx.strokeText(d.id, d.x, d.y - 10);
+        self.ctx.fillStyle = '#000';
+        self.ctx.fillText(d.id, d.x, d.y - 10);
+      });
+      self.ctx.restore();
+    }
 
-  	this.node = this.svg.append("g")
-				.attr("fill", "currentColor")
-				.attr("stroke-linecap", "round")
-				.attr("stroke-linejoin", "round")
-				.selectAll("g")
-				.data(this.nodes)
-				.join("g")
-				.call(this.drag(this.simulation));
+    function zoomed() {
+      self.transform = d3.event.transform;
+      simulationUpdate();
+    }
 
-  	this.node.append("circle")
-      .attr("stroke", "white")
-      .attr("stroke-width", 1.5)
-      .attr("r", 4);
+    /** Find the node that was clicked, if any, and return it. */
+    function dragSubject() {
+      const x = self.transform.invertX(d3.event.x);
+      const y = self.transform.invertY(d3.event.y);
+      const node = self.findNode(self.nodes, x, y, self.nodeRadius+5);
+      if (node) {
+        node.x =  self.transform.applyX(node.x);
+        node.y = self.transform.applyY(node.y);
+      }
+      // else: No node selected, drag container
+      return node;
+    }
 
-  	this.node.append("text")
-      .attr("x", 8)
-      .attr("y", "0.31em")
-      .text(d => d.id)
-    .clone(true).lower()
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-width", 3);
+    function dragStarted() {
+      if (!d3.event.active) {
+        self.simulation.alphaTarget(0.3).restart();
+      }
+      d3.event.subject.fx = self.transform.invertX(d3.event.x);
+      d3.event.subject.fy = self.transform.invertY(d3.event.y);
+    }
 
-  	this.simulation.on("tick", () => {
-    	this.link.attr("d", this.linkArc);
-    	this.node.attr("transform", d => `translate(${d.x},${d.y})`);
-  	});
+    function dragged() {
+      d3.event.subject.fx = self.transform.invertX(d3.event.x);
+      d3.event.subject.fy = self.transform.invertY(d3.event.y);
+    }
 
-
-  	//this.invalidation.then(() => this.simulation.stop());
-  	//return this.svg.node();
+    function dragEnded() {
+      if (!d3.event.active) {
+        self.simulation.alphaTarget(0);
+      }
+      d3.event.subject.fx = null;
+      d3.event.subject.fy = null;
+    }
   }
 
-  drag(simulation){
-		const dragstarted = function(d) {
-		  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-		  d.fx = d.x;
-		  d.fy = d.y;
-		}
-  
-		const dragged = function(d) {
-		  d.fx = d3.event.x;
-		  d.fy = d3.event.y;
-		}
-  
-		const dragended = function(d) {
-		    if (!d3.event.active) simulation.alphaTarget(0);
-		    d.fx = null;
-		    d.fy = null;
-  	}
-  
-  	return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
+  findNode(nodes, x, y, radius) {
+    const rSq = radius * radius;
+    let i;
+    for (i = nodes.length - 1; i >= 0; --i) {
+      const node = nodes[i];
+      const dx = x - node.x;
+      const dy = y - node.y;
+      const distSq = (dx * dx) + (dy * dy);
+      if (distSq < rSq) {
+        return node;
+      }
+    }
+    // No node selected
+    return undefined;
   }
 
-  linkArc(d) {
-  	const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
-  	return `
-    			M${d.source.x},${d.source.y}
-    			A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
-  				`;
-	}
+  ///////////////////////
 
-	private initData(){
-		// this.links = LINKS.map(d => Object.create(d));
-	 //  this.nodes = NODES.map(d => Object.create(d));
-		// this.types = ['licensing', 'suit', 'resolved']
-	}
-
-	private refreshGraph(param){
-
-		this.showGraph = false;
-
-
-		if(! (this.data && this.data.length)) return;
-
-		this.types = Array.from(new Set( this.data.map(d=> d['type']) ));
-
-		let _source;
-
-		if(param === 'no_definido'){
-			_source = this.data;
-			this.defaultView = true;
-
-		} else {
-			this.defaultView = false;
-			_source = this.data.filter(token => {
-				return token.type === param;
-			})
-
-		}
-
-		let _nodes = Array.from(new Set( _source.flatMap(l => [l.source, l.target])), id =>({ id }) )
-		this.nodes = _nodes.map(d => Object.create(d));
-
-		this.links = _source.map(d => Object.create(d));
-
-		this.showGraph = true;
-		this.resetGraph()
-
-	}
-
-
-
+  private refreshGraph(param){
+    this.showGraph = false;
+    if(! (this.data && this.data.length)) return;
+    this.types = Array.from(new Set( this.data.map(d => d['type']) ));
+    let _source;
+    if(param === 'no_definido'){
+      _source = this.data;
+      this.defaultView = true;
+    } else {
+      this.defaultView = false;
+      _source = this.data.filter(token => {
+        return token.type === param;
+      });
+    }
+    const _nodes = Array.from(new Set( _source.flatMap(l => [l.source, l.target])), id => ({ id }) );
+    this.nodes = _nodes.map(d => Object.create(d));
+    this.links = _source.map(d => Object.create(d));
+    this.showGraph = true;
+    this.forceGraph();
+  }
 }
-
-const NODES = [
-		{ id: 'Amazon'},
-		{ id: 'Apple'},
-		{ id: 'Barnes & Noble'},
-		{ id: 'Ericsson'},
-		{ id: 'Foxconn'},
-		{ id: 'Google'},
-		{ id: 'HTC'},
-		{ id: 'Huawei'},
-		{ id: 'Inventec'},
-		{ id: 'Kodak'},
-		{ id: 'LG'},
-		{ id: 'Microsoft'},
-		{ id: 'Motorola'},
-		{ id: 'Nokia'},
-		{ id: 'Oracle'},
-		{ id: 'Qualcomm'}, 
-		{ id: 'RIM'},
-		{ id: 'Samsung'},
-		{ id: 'Sony'},
-		{ id: 'ZTE'},
-]
-
-
-const LINKS = [
-		{ source: 'Microsoft',  target:  'Amazon',  type: 'licensing'},
-		{ source: 'Microsoft',  target:  'HTC',     type: 'licensing'},
-		{ source: 'Samsung',    target:  'Apple',   type: 'suit'},
-		{ source: 'Motorola',   target:  'Apple',   type: 'suit'},
-		{ source: 'Nokia',      target:  'Apple',   type: 'resolved'},
-		{ source: 'HTC',        target:  'Apple',   type: 'suit'},
-		{ source: 'Kodak',      target:  'Apple',   type: 'suit'},
-		{ source: 'Microsoft',  target:  'Barnes & Noble', type: 'suit'},
-		{ source: 'Microsoft',  target:  'Foxconn', type: 'suit'},
-		{ source: 'Oracle',     target:  'Google',  type: 'suit'},
-		{ source: 'Apple',      target:  'HTC',     type: 'suit'},
-		{ source: 'Microsoft',  target:  'Inventec',  type: 'suit'},
-		{ source: 'Samsung',    target:  'Kodak',   type: 'resolved'},
-		{ source: 'LG',         target:  'Kodak',   type: 'resolved'},
-		{ source: 'RIM',        target:  'Kodak',   type: 'suit'},
-		{ source: 'Sony',       target:  'LG',      type: 'suit'},
-		{ source: 'Kodak',      target:  'LG',      type: 'resolved'},
-		{ source: 'Apple',      target:  'Nokia',   type: 'resolved'},
-		{ source: 'Qualcomm',   target:  'Nokia',   type: 'resolved'},
-		{ source: 'Apple',      target:  'Motorola',  type: 'suit'},
-		{ source: 'Microsoft',  target:  'Motorola',  type: 'suit'},
-		{ source: 'Motorola',   target:  'Microsoft', type: 'suit'},
-		{ source: 'Huawei',     target:  'ZTE',      type: 'suit'},
-		{ source: 'Ericsson',   target:  'ZTE',      type: 'suit'},
-		{ source: 'Kodak',      target:  'Samsung',  type: 'resolved'},
-		{ source: 'Apple',      target:  'Samsung',  type: 'suit'},
-		{ source: 'Kodak',      target:  'RIM',      type: 'suit'},
-		{ source: 'Nokia',      target:  'Qualcomm', type: 'suit'},
-]
-
-
