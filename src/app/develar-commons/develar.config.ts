c5d03594bbb23c412b8a50bd1c985746d3630038
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-08-20 09:43:21 -0300

    salud ajustes informe epidemio 02

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #34 20.08.18-18:49',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #34 20.08.20-09:42',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
