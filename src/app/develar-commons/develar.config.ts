ddad83da4674bd50e4fe852b4acc9073c5e6601c
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-12-07 16:13:28 -0300

    industras ajuste deploy 06

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -112,7 +112,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Producción - Almirante Brown  - SEMANA #33 20.12.07-14:17',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Producción - Almirante Brown  - SEMANA #33 20.12.07-16:13',
 		version: '1.0 ',
 		url: 'industrias.brown.gob.ar'
 	},
