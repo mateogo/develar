5b628023f8de80b87b674f282e3051067fc8f5a8
Mateo Gomez Ortega <mgomezortega@gmail.com>
2020-12-08 11:29:27 -0300

    salud update programas

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -4,7 +4,7 @@
  *
  *
 *************/
-const user = 'industrias';
+const user = 'salud';
 
 const globals = {
 
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #37 20.10.28-20:59',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #39 20.12.08-11:27',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
