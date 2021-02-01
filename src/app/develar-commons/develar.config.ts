427d90cf0ab56112d3151ff215d2181a80fea50a
Mateo Gomez Ortega <mgomezortega@gmail.com>
2021-02-01 12:37:00 -0300

    salud reactivar edicion direccion en form vinculos, por qué estaba inhabilitado?

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #2 21.02.01-11:37',
+		copyright: '©2020 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  - SEMANA EPIDEMIOLÓGICA #2 21.02.01-12:35',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
