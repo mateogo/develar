ca585c1fa5a29eaa48d0870b86d914288225206f
Mateo Gomez Ortega <mgomezortega@gmail.com>
2021-04-05 14:45:21 -0300

    dsocial reemplazo Delegacion Glew por CIC Glew en la entrega de alimentos

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -4,7 +4,7 @@
  *
  * @
 *************/
-const user = 'salud';
+const user = 'dsocial';
 
 const globals = {
 
@@ -21,7 +21,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: 'DGTS@modernización - Secretaría General - Secretaría de Desarrollo Social - Almirante Brown 21.03.30 - 06:13',
+		copyright: 'DGTS@modernización - Secretaría General - Secretaría de Desarrollo Social - Almirante Brown 21.04.05 - 14:27',
 		version: '1.0 beta',
 		url: 'dsocial.brown.gob.ar'
 	},
