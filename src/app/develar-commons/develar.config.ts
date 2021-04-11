e30fbde04c532ad330b13fa40fae5a06a57f727e
Mateo Gomez Ortega <mgomezortega@gmail.com>
2021-04-10 21:46:08 -0300

    salud ajuste esquema de seguimiento 21:45 02

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2021 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  -  21.04.10-21:28',
+		copyright: '©2021 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  -  21.04.10-21:45',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
