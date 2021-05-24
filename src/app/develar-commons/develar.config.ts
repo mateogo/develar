babcf8244e8e5537f0cf29fb7f7c25030edb2278
Mateo Gomez Ortega <mgomezortega@gmail.com>
2021-05-24 18:31:56 -0300

    salud workload  in progress 13

--- src/app/develar-commons/develar.config.ts
+++ src/app/develar-commons/develar.config.ts
@@ -39,7 +39,7 @@ const globals = {
 		socialmedia: 'DEFAULT_SOCIAL_MEDIA',
 		emailsubject: '[MAB] Contacto',
 		emailbody: 'Formulario de contacto completado',
-		copyright: '©2021 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  -  21.05.23-19:29',
+		copyright: '©2021 DGTS@modernización - Secretaría General - Secretaría de Salud - Almirante Brown  -  21.05.24-18:31',
 		version: '1.0 beta',
 		url: 'salud.brown.gob.ar'
 	},
